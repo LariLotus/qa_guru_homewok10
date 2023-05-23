e7b61bb6437462ebd8b2f69a21fa79c904662361
Larisa Badmaeva <larilotus12@gmail.com>
2023-05-23 16:40:04 +0300

    add tests

---
+++ tests/test_selene.py
@@ -0,0 +1,18 @@
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open("https://github.com")
+
+    browser.element(".header-search-input").click()
+    browser.element(".header-search-input").send_keys("eroshenkoam/allure-example")
+    browser.element(".header-search-input").submit()
+
+    browser.element(by.link_text("eroshenkoam/allure-example")).click()
+
+    browser.element("#issues-tab").click()
+
+    browser.element(by.partial_text("#76")).should(be.visible)
